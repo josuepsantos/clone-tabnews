391144e17613468d362ee096fde2ce7cc6521b61
Josué Pereira dos Santos <140112369+josuepsantos@users.noreply.github.com>
2024-12-03 19:27:48 +0000

    Adicionando um index para resolução do desafio01, através do gitpages

---
+++ desafio/index.js
@@ -0,0 +1,1 @@
+document.getElementById('titulo').innerText = 'Qual seu anime favorito?';
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Se você consegue ler isso, qual seu anime favorito?</h1>
+}
+
+export default Home;
